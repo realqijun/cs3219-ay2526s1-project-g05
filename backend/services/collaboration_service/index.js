fb713621aa8293d6abafc2862d61cf47e3ff1b6b
Vatinius <102.23.dylan.chan@gmail.com>
2025-09-26 15:53:15 +0800

    Update ports for collaboration service

--- backend/services/collaboration_service/index.js
+++ backend/services/collaboration_service/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 
+const port = process.env.COLLABORATIONSERVICEPORT || 4004;
+
 const app = express();
 
 app.get("/status", (req, res) => {
   res.send("Collaboration service is up and running!");
 });
 
-app.listen(3000, () => {
-  console.log("Collaboration service is running on port 3000");
+app.listen(port, () => {
+  console.log(`Collaboration service is running on port ${port}`);
 });

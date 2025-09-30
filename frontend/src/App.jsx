ef7375b303672fbb77eaacd36e7f442a8d528731
Deborah Lu <deborahluyx@gmail.com>
2025-09-30 15:20:15 +0800

    add 404 and error routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import { Navbar } from './components/Navbar'
-import { Hero } from './components/Hero'
-import { Footer } from './components/Footer'
+import { Navbar } from '@/components/Navbar'
+import { Hero } from '@/components/Hero'
+import { Footer } from '@/components/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />

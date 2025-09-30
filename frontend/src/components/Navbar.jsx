a8a79dbb726a3a6c85a3e6ec79951e6130882895
Deborah Lu <deborahluyx@gmail.com>
2025-09-30 10:31:00 +0800

    add landing page

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { buttonVariants } from "./ui/button";
+import { Menu } from "lucide-react";
+import { ModeToggle } from "./mode-toggle";
+import logo from "@/assets/logo.png";
+
+const routeList = [
+  { href: "#explore", label: "Explore" },
+  { href: "#problems", label: "Problems" },
+  { href: "#discuss", label: "Discuss" },
+];
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
+      <NavigationMenu className="mx-auto">
+        <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between ">
+          <NavigationMenuItem className="font-bold flex">
+            <a rel="noreferrer noopener" href="/" className="ml-2 font-bold text-xl flex">
+               <img
+                src={logo}
+                style={{ height: '55px' }}
+                alt="logo"
+              />
+            </a>
+          </NavigationMenuItem>
+
+          {/* mobile */}
+          <span className="flex md:hidden">
+            <ModeToggle />
+
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger className="px-2">
+                <Menu className="flex md:hidden h-5 w-5" onClick={() => setIsOpen(true)}>
+                  <span className="sr-only">Menu Icon</span>
+                </Menu>
+              </SheetTrigger>
+
+              <SheetContent side={"left"}>
+                <SheetHeader>
+                  <SheetTitle className="font-bold text-xl">Shadcn/React</SheetTitle>
+                </SheetHeader>
+                <nav className="flex flex-col justify-center items-center gap-2 mt-4">
+                  {routeList.map(({ href, label }) => (
+                    <a
+                      rel="noreferrer noopener"
+                      key={label}
+                      href={href}
+                      onClick={() => setIsOpen(false)}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      {label}
+                    </a>
+                  ))}
+                  <a
+                    rel="noreferrer noopener"
+                    href="#login"
+                    target="_blank"
+                    className={`w-[110px] border ${buttonVariants({ variant: "secondary" })}`}
+                  >
+                    Log In
+                  </a>
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </span>
+
+          {/* desktop */}
+          <nav className="hidden md:flex gap-2">
+            {routeList.map(({ href, label }, i) => (
+              <a
+                rel="noreferrer noopener"
+                href={href}
+                key={i}
+                className={`text-[17px] ${buttonVariants({ variant: "ghost" })}`}
+              >
+                {label}
+              </a>
+            ))}
+          </nav>
+
+          <div className="hidden md:flex gap-2">
+            <a
+              rel="noreferrer noopener"
+              href="#login"
+              target="_blank"
+              className={`border ${buttonVariants({ variant: "secondary" })}`}
+            >
+              Log In
+            </a>
+
+            {/* <ModeToggle /> */}
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};

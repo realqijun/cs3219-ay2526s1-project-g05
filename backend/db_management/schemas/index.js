4240d330b635918898af75d64eddbf531b379494
tkaixiang <tkaixiang@gmail.com>
2025-10-19 16:06:11 +0800

    Add collaboration_sessions schemas

--- backend/db_management/schemas/index.js
+++ backend/db_management/schemas/index.js
@@ -1,5 +1,9 @@
 import { question_schema, question_indexes } from "./questions.js";
 import { users_schema, users_indexes } from "./users.js";
+import {
+  collaboration_sessions_schema,
+  collaboration_sessions_indexes,
+} from "./collaboration_sessions.js";
 
 // Add your schemas here!
 export const COLLECTIONS = {
@@ -10,5 +14,9 @@ export const COLLECTIONS = {
   users: {
     schema: users_schema,
     indexes: users_indexes,
+  },
+  collaboration_sessions: {
+    schema: collaboration_sessions_schema,
+    indexes: collaboration_sessions_indexes,
   },
 };
